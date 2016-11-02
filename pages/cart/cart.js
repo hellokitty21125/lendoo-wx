22e1986eb0a0658edef8f062a33a33b86c566638
huangxiujie <huangxiujie@163.com>
2016-11-02 22:55:58 +0800

    cart fixed bug

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -9,7 +9,7 @@ Page({
 	},
 	bindMinus: function(e) {
 		var index = parseInt(e.currentTarget.dataset.index);
-		var num = this.data.carts[index].num;
+		var num = this.data.carts[index].get('quantity');
 		// 如果只有1件了，就不允许再减了
 		if (num > 1) {
 			num --;
@@ -18,7 +18,7 @@ Page({
 		var minusStatus = num <= 1 ? 'disabled' : 'normal';
 		// 购物车数据
 		var carts = this.data.carts;
-		carts[index].num = num;
+		carts[index].set('quantity', num);
 		// 按钮可用状态
 		var minusStatuses = this.data.minusStatuses;
 		minusStatuses[index] = minusStatus;
@@ -27,18 +27,20 @@ Page({
 			carts: carts,
 			minusStatuses: minusStatuses
 		});
+		// update database
+		carts[index].save();
 		this.sum();
 	},
 	bindPlus: function(e) {
 		var index = parseInt(e.currentTarget.dataset.index);
-		var num = this.data.carts[index].num;
+		var num = this.data.carts[index].get('quantity');
 		// 自增
 		num ++;
 		// 只有大于一件的时候，才能normal状态，否则disable状态
 		var minusStatus = num <= 1 ? 'disabled' : 'normal';
 		// 购物车数据
 		var carts = this.data.carts;
-		carts[index].num = num;
+		carts[index].set('quantity', num);
 		// 按钮可用状态
 		var minusStatuses = this.data.minusStatuses;
 		minusStatuses[index] = minusStatus;
@@ -47,17 +49,20 @@ Page({
 			carts: carts,
 			minusStatuses: minusStatuses
 		});
+		// update database
+		carts[index].save();
 		this.sum();
 	},
 	bindManual: function(e) {
 		var index = parseInt(e.currentTarget.dataset.index);
 		var carts = this.data.carts;
 		var num = e.detail.value;
-		carts[index].num = num;
+		carts[index].set('quantity', num);
 		// 将数值与状态写回
 		this.setData({
 			carts: carts
 		});
+		cart[index].save();
 		console.log(this.data.carts);
 	},
 	bindCheckbox: function(e) {
@@ -65,14 +70,16 @@ Page({
 		//拿到下标值，以在carts作遍历指示用
 		var index = parseInt(e.currentTarget.dataset.index);
 		//原始的icon状态
-		var selected = this.data.carts[index].selected;
+		var selected = this.data.carts[index].get('selected');
 		var carts = this.data.carts;
 		// 对勾选状态取反
-		carts[index].selected = !selected;
+		carts[index].set('selected', !selected);
 		// 写回经点击修改后的数组
 		this.setData({
 			carts: carts,
 		});
+		// update database
+		carts[index].save();
 		this.sum();
 	},
 	bindSelectAll: function() {
@@ -84,7 +91,9 @@ Page({
 		var carts = this.data.carts;
 		// 遍历
 		for (var i = 0; i < carts.length; i++) {
-			carts[i].selected = selectedAllStatus;
+			carts[i].set('selected', selectedAllStatus);
+			// update selected status to db
+			carts[i].save();
 		}
 		this.setData({
 			selectedAllStatus: selectedAllStatus,
@@ -123,15 +132,11 @@ Page({
 		}, function () {
 		});
 	},
-	bindToastChange: function() {
-		this.setData({
-			toastHidden: true
-		});
-	},
 	onLoad: function() {
 		var that = this;
 		var user = AV.User.current();
 		var query = new AV.Query('Cart');
+		var minusStatuses = [];
 		query.equalTo('user',user);
 		query.include('goods');
 		query.find().then(function (carts) {
@@ -140,22 +145,25 @@ Page({
 			for(var i = 0; i < carts.length; i++){
 				var goods = carts[i].get('goods');
 				goodsList[i] = goods;
+				minusStatuses[i] = carts[i].get('quantity') <= 1 ? 'disabled' : 'normal';
 			}
-			console.log(goodsList);
+			// console.log(carts);
 			that.setData({
 				carts: carts,
-				goodsList: goodsList
+				goodsList: goodsList,
+				minusStatuses: minusStatuses
 			});
+			// sum
+			that.sum();
 		});
-		this.sum();
 	},
 	sum: function() {
 		var carts = this.data.carts;
 		// 计算总金额
 		var total = 0;
 		for (var i = 0; i < carts.length; i++) {
-			if (carts[i].selected) {
-				total += carts[i].num * carts[i].price;
+			if (carts[i].get('selected')) {
+				total += carts[i].get('quantity') * carts[i].get('goods').get('price');
 			}
 		}
 		// 写回经点击修改后的数组
