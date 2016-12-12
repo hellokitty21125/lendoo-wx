787dbf39e38432bf2a0fe1201754d7818e00ea10
huangxiujie <huangxiujie@163.com>
2016-12-12 22:29:42 +0800

    avatar upload

--- pages/member/index/index.js
+++ pages/member/index/index.js
@@ -25,10 +25,37 @@ Page({
 		// 调用小程序 API，得到用户信息
 		wx.getUserInfo({
 			success: ({userInfo}) => {
-  				that.setData({
-				userInfo: userInfo
-			});
-		  }
+				that.setData({
+					userInfo: userInfo
+				});
+			}
 		});
+	},
+	chooseImage: function () {
+		var that = this;
+		wx.chooseImage({
+			count: 1, // 默认9
+			sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+			sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+			success: function (res) {
+				// 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+				var tempFilePath = res.tempFilePaths[0];
+				new AV.File('file-name', {
+					blob: {
+						uri: tempFilePath,
+					},
+				}).save().then(
+				// file => console.log(file.url())
+					function(file) {
+						// 上传成功后，将所上传的头像设置更新到页面<image>中
+						var userInfo = that.data.userInfo;
+						userInfo.avatarUrl = file.url();
+						that.setData({
+							userInfo, userInfo
+						});
+					}
+				).catch(console.error);
+			}
+		})
 	}
 })
