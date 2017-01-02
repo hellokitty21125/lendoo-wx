e1cc948fbf804fc1a38e700a3ada51c75396f266
huangxiujie <huangxiujie@163.com>
2017-01-02 22:47:55 +0800

    wx.login

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // 初始化AV
 const AV = require('./utils/av-weapp.js')
+const appId = "SgHcsYqoLaFTG0XDMD3Gtm0I-gzGzoHsz";
+const appKey = "xdv2nwjUK5waNglFoFXkQcxP";
+
 AV.init({ 
-    appId: "SgHcsYqoLaFTG0XDMD3Gtm0I-gzGzoHsz", 
-    appKey: "xdv2nwjUK5waNglFoFXkQcxP",
+    appId: appId, 
+    appKey: appKey,
 });
 
 // 授权登录
@@ -11,5 +14,29 @@ App({
         // auto login via SDK
         var that = this;
         AV.User.loginWithWeapp();
+        
+        var user = AV.User.current();
+        // console.log(user.get('username'));
+		wx.login({
+	      success: function(res) {
+	        if (res.code) {
+	          //发起网络请求
+	          wx.request({
+	            url: 'https://api.weixin.qq.com/sns/jscode2session?appid=wx9114b997bd86f8ed&secret=d27551c7803cf16015e536b192d5d03b&js_code='+res.code+'&grant_type=authorization_code',
+	            data: {
+	              code: res.code
+	            },
+	            success: function (response) {
+	            	console.log(response);
+	            }
+	          })
+	        } else {
+	          console.log('获取用户登录态失败！' + res.errMsg)
+	        }
+	      }
+	    });
+
+
+
     }
 })
