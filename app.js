d83cd78eadf33ca3617f67febd9320216f43055c
huangxiujie <huangxiujie@163.com>
2017-01-03 11:08:33 +0800

    clear comment

--- app.js
+++ app.js
@@ -37,17 +37,19 @@ App({
 	            			openId: openId
 	            		},
 	            		method: 'POST',
+	            		header: {
+						    'content-type': 'application/json'
+						},
 	            		success: function (response) {
-	            			console.log('pay:');
 	            			console.log(response);
 			            	// 发起支付
-			            	var appId = 'wx9114b997bd86f8ed';
-			            	// var appId = response.data.appid;
+			            	// var appId = 'wx9114b997bd86f8ed';
+			            	var appId = response.data.appid;
 			            	var timeStamp = (Date.parse(new Date()) / 1000).toString();
-			            	var pkg = 'prepay_id=' + 'wx2017010310144508a4f7b72b0255261470';
-			            	var nonceStr = 'MauUXdes5ni9wjis';
-			            	// var pkg = 'prepay_id=' + response.data.prepay_id;
-			            	// var nonceStr = response.data.nonce_str;
+			            	// var pkg = 'prepay_id=' + 'wx2017010310144508a4f7b72b0255261470';
+			            	// var nonceStr = 'MauUXdes5ni9wjis';
+			            	var pkg = 'prepay_id=' + response.data.prepay_id;
+			            	var nonceStr = response.data.nonce_str;
 			            	console.log('appId='+appId+'&nonceStr='+nonceStr+'&package='+pkg+'&signType=MD5&timeStamp='+timeStamp);
 			            	var paySign = md5.hex_md5('appId='+appId+'&nonceStr='+nonceStr+'&package='+pkg+'&signType=MD5&timeStamp='+timeStamp+"&key=d27551c7803cf16015e536b192d5d03b").toUpperCase();
 			            	console.log(paySign);
@@ -60,10 +62,6 @@ App({
 								'paySign': paySign,
 								'success':function(res){
 									console.log('success');
-									console.log(res);
-								},
-								'fail':function(res){
-									console.log('error');
 									console.log(res);
 								}
 							});
