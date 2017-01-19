c2526fe21a1b9d1ca37f54bc4aab7c972cbb5257
iMac <huangxiujie@163.com>
2017-01-19 10:06:59 +0800

    试图实现购买页支付

--- pages/order/payment/payment.js
+++ pages/order/payment/payment.js
@@ -5,34 +5,50 @@ Page({
 	},
 	onLoad: function (options) {
 		var orderId = options.orderId;
-		console.log('order id : ' + orderId);
+		var totalFee = options.totalFee;
 		this.setData({
-			orderId: orderId
+			orderId: orderId,
+			totalFee: totalFee
 		})
 	},
 	pay: function () {
-			// wx.requestPayment({
-			//    'timeStamp': '',
-			//    'nonceStr': '',
-			//    'package': '',
-			//    'signType': 'MD5',
-			//    'paySign': '',
-			//    'success':function(res){
-			//    		console.log(res);
-			//    },
-			//    'fail':function(res){
-			//    		console.log(res);
-			//    }
-			// })
-
-			// update order
-			var query = new AV.Query('Order');
-			query.get(this.data.orderId).then(function (order) {
-				order.set('status', 1);
-				order.save();
-				console.log('status: ' + 1);
-			}, function (err) {
-				
-			});
+		var that = this;
+		console.log(getApp().code);
+		//统一下单接口对接
+		wx.request({
+			url: 'https://lendoo.leanapp.cn/index.php/WXPay',
+			data: {
+				code: getApp().code,
+				body: '灵动商城',
+				tradeNo: that.data.orderId,
+				totalFee: parseInt(that.data.totalFee)
+			},
+			method: 'POST',
+			header: {
+				'content-type': 'application/x-www-form-urlencoded'
+			},
+			success: function (response) {
+				console.log(response);
+				// 发起支付
+				wx.requestPayment({
+					'timeStamp': response.data.timeStamp,
+					'nonceStr': response.data.nonceStr,
+					'package': response.data.package,
+					'signType': 'MD5',
+					'paySign': response.data.paySign,
+					'success':function(res){
+						// update order
+						var query = new AV.Query('Order');
+						query.get(that.data.orderId).then(function (order) {
+							order.set('status', 1);
+							order.save();
+							console.log('status: ' + 1);
+						}, function (err) {
+							
+						});
+					}
+				});
+			}
+		});
 	}
 })
