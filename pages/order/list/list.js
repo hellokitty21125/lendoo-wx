b2291ade9c036464e2ca4caf39c2a89a777cfaf9
huangxiujie <huangxiujie@163.com>
2017-01-22 00:04:24 +0800

    我的订单遍历读取，使用了Order表的objectId对应绑定解决异步请求所带来的数据不一致问题

--- pages/order/list/list.js
+++ pages/order/list/list.js
@@ -10,45 +10,45 @@ Page({
 		var query = new AV.Query('Order');	
 		query.include('buys');
 		query.equalTo('user', user);
+		query.descending('createdAt');
 		query.find().then(function (orderObjects) {
 			that.setData({
-				'orders': orderObjects
+				orders: orderObjects
 			});
 			// loop search order, fetch the Buy objects
 			for (var i = 0; i < orderObjects.length; i++) {
-				var order = AV.Object.createWithoutData('Order', orderObjects[i].get('objectId'));
-				// order = orderObjects[i]; 竟然不能使用这种方式取出目标对象，而只能使用createWithoutData的方式，LeanCloud的做法让人匪夷所思。
-				var relation = order.relation('buys');
-				var queryBuy = relation.query();
-				queryBuy.include('goods');
-				queryBuy.find().then(function (buyObjects) {
-					console.log(buyObjects[0]);
-					for (var j = 0; j < buyObjects.length; j++) {
-						var buyObject = buyObjects[j];
-						var buysData = that.data.buysData == undefined ? [] : that.data.buysData;
-						var buy = {
-							avatar: buyObject.get('goods').get('avatar'),
-							title: buyObject.get('goods').get('title'),
-							price: buyObject.get('goods').get('price'),
-							quantity: buyObject.get('quantity')
+				var order = orderObjects[i];
+				var queryMapping = new AV.Query('OrderGoodsMap');
+				queryMapping.include('goods');
+				queryMapping.equalTo('order', order);
+				queryMapping.find().then(function (mappingObjects) {
+					var mappingArray = [];
+					for (var j = 0; j < mappingObjects.length; j++) {
+						var mappingObject = mappingObjects[j];
+						var mapping = {
+							avatar: mappingObject.get('goods').get('avatar'),
+							title: mappingObject.get('goods').get('title'),
+							price: mappingObject.get('goods').get('price'),
+							quantity: mappingObject.get('quantity')
 						};
-						buysData[j] = buy;
-						// console.log(buy);
-						that.setData({
-							buysData: buysData
-						});
-						console.log('i:' + i);
-						console.log('buysData');
-						// console.log(that.data.buysData);
-						// console.log(buyObject.get('objectId'));
-						// console.log(buyObject.get('goods').get('avatar'));
-						// console.log(buyObject.get('goods').get('title'));
-						// console.log(buyObject.get('goods').get('price'));
-						// console.log(buyObject.get('quantity'));
-
+						mappingArray.push(mapping);
 					}
-				}, function (error) {
-					console.log(error);
+					// 找出orderObjectId所在的索引位置，来得到k的值
+					var k = 0;
+					var orders = that.data.orders;
+					for (var index = 0; index < orders.length; index++) {
+						var order = orders[index];
+						if (order.get('objectId') == mappingObject.get('order').get('objectId')) {
+							k = index;
+							console.log('k: ' + k);
+							break;
+						}
+					}
+					var mappingData = that.data.mappingData == undefined ? [] : that.data.mappingData;
+					mappingData[k] = mappingArray;
+					that.setData({
+						mappingData: mappingData
+					});
 				});
 			}
 		});
