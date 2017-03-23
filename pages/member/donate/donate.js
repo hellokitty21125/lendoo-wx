1f4e699f9682168771ac549637c8c1e9810e59c0
huangxiujie <huangxiujie@126.com>
2017-03-23 15:42:04 +0800

    初始值极小值

--- pages/member/donate/donate.js
+++ pages/member/donate/donate.js
@@ -1,8 +1,10 @@
 var that;
 var deltaX = 0;
+var minValue = 1;
+var app = getApp();
 Page({
 	data: {
-		amount: 5,
+		value: 0,
 		canvasHeight: 80
 	},
 	onLoad: function (options) {
@@ -16,19 +18,30 @@ Page({
 
 		/* 1.定义变量 */
 
-		// 1.1 定义原点与终点，x轴方向起点与终点各留100px空白
-		var origion = {x: 100, y: this.data.canvasHeight};
-		var end = {x: 900, y: this.data.canvasHeight};
+		// 1.1 定义原点与终点，x轴方向起点与终点各留半屏空白
+		var origion = {x: app.screenWidth / 2, y: that.data.canvasHeight};
+		var end = {x: app.screenWidth / 2, y: that.data.canvasHeight};
 		// 1.2 定义刻度线高度
 		var heightDecimal = 50;
 		var heightDigit = 25;
 		// 1.3 定义文本标签字体大小
 		var fontSize = 20;
-		// 1.4 总刻度
+		// 1.4 最小刻度值
+		// 已经定义在全局，便于bindscroll访问
+		// 1.5 总刻度值
 		var maxValue = 200;
-		// 1.5 每个刻度所占位的px
+		// 1.6 当前刻度值
+		var currentValue = 10;
+		// 1.7 每个刻度所占位的px
 		var ratio = 10;
-		
+		// 1.8 画布宽度
+		var canvasWidth = maxValue * ratio + app.screenWidth - minValue * ratio;
+		// 设定scroll-view初始偏移
+		that.setData({
+			canvasWidth: canvasWidth,
+			scrollLeft: (currentValue - minValue) * ratio
+		});
+
 		/* 2.绘制 */
 
 		// 2.1初始化context
@@ -37,9 +50,9 @@ Page({
 		for (var i = 0; i <= maxValue; i++) {
 			context.beginPath();
 			// 2.2 画刻度线
-			context.moveTo(origion.x + i * ratio, origion.y);
+			context.moveTo(origion.x + (i - minValue) * ratio, origion.y);
 			// 画线到刻度高度，10的位数就加高
-			context.lineTo(origion.x + i * ratio, origion.y - (i % ratio == 0 ? heightDecimal : heightDigit));
+			context.lineTo(origion.x + (i - minValue) * ratio, origion.y - (i % ratio == 0 ? heightDecimal : heightDigit));
 			// 设置属性
 			context.setLineWidth(2);
 			// 10的位数就加深
@@ -51,7 +64,7 @@ Page({
 			if (i % ratio == 0) {
 				context.setFontSize(fontSize);
 				// 为零补一个空格，让它看起来2位数，页面更整齐
-				context.fillText(i == 0 ? ' ' + i : i, origion.x + i * ratio - fontSize / 2, fontSize);
+				context.fillText(i == 0 ? ' ' + i : i, origion.x + (i - minValue) * ratio - fontSize / 2, fontSize);
 			}
 			context.closePath();
 		}
@@ -62,7 +75,7 @@ Page({
 	drawCursor: function () {
 		/* 定义变量 */
 		// 定义三角形顶点 TODO x
-		var center = {x: 170, y: 0};
+		var center = {x: app.screenWidth / 2, y: 5};
 		// 定义三角形边长
 		var length = 20;
 		// 左端点
@@ -83,9 +96,9 @@ Page({
 		// deltaX 水平位置偏移位，每次滑动一次触发一次，所以需要记录从第一次触发滑动起，一共滑动了多少距离
 		deltaX += e.detail.deltaX;
 		// 数据绑定
-		// that.setData({
-		// 	amount: - deltaX / 100
-		// });
+		that.setData({
+			value: Math.floor(- deltaX / 10 + minValue)
+		});
 		console.log(deltaX)
 	}
 });
